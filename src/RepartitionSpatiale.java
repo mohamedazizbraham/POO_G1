1481f5dfe4523a0adb1880c63fd32be1b775f219
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-11-29 12:18:44 +0100

    commit de RepartitionSpatiale

---
+++ src/RepartitionSpatiale.java
@@ -0,0 +1,10 @@
+public class RepartitionSpatiale {
+    private float dMax;
+    private float p0;
+
+    public RepartitionSpatiale(float dMax, float p0) {
+        this.dMax = dMax;
+        this.p0 = p0;
+    }
+
+}
