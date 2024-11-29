a9c24044ebd7f3ed404332ce708a6fd92bc2e921
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-11-29 12:06:30 +0100

    creation de la class individu

--- src/Individu.java
+++ src/Individu.java
@@ -1,7 +1,19 @@
 public class Individu {
-    public int cycleResistance;
-    public float x ;
-    public float y;
-    public boolean infecter ;
-    public boolean guerie;
-}
+private Etat etat;
+private sensibilité sensibilité;
+private int cycleResistance;
+private float x;
+private float y;
+private String sensitivite;
+private boolean infecter;
+private boolean guerie;
+
+public Individu(Etat etat, float x, float y, String sensitivite) {
+    this.etat = etat;
+    this.x = x;
+    this.y = y;
+    this.sensitivite = sensitivite;
+    this.infecter = false;
+    this.guerie = false;
+    this.cycleResistance = 0;
+}}
