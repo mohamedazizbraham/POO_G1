ec44157da664b4f44d0c92af82e1d44a130965c3
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-03 16:40:49 +0100

    commit de Etat

---
+++ src/ComportementSpatiale.java
@@ -0,0 +1,10 @@
+public class ComportementSpatiale {
+    private boolean distanciation;
+    private boolean protection;
+
+    public ComportementSpatiale() {
+        this.distanciation = distanciation;
+        this.protection = protection;
+    }
+
+}
---
+++ src/Etat.java
@@ -0,0 +1,3 @@
+enum Etat {
+    Infecté,Sain,Guéri,Décédé
+}
