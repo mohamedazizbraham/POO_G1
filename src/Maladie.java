9aaa4859c611c86a161a1a9a2eaa0418f829d833
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-03 17:02:49 +0100

    modification de Maladie

--- src/Maladie.java
+++ src/Maladie.java
@@ -1,15 +1,21 @@
-public class Epidemi {
+public class Maladie {
     private String nom;
     private float tauxTransmission;
     private float probaGuerison;
     private int periodeIncubation;
     private double probaDeces;
+    private float dMax;
+    private float p0;
 
-    public Epidemie(String nom, float tauxTransmission, float probaGuerison, int periodeIncubation, double probaDeces) {
+    public Maladie(String nom, float tauxTransmission, float probaGuerison, int periodeIncubation, double probaDeces, float dMax, float p0) {
         this.nom = nom;
         this.tauxTransmission = tauxTransmission;
         this.probaGuerison = probaGuerison;
         this.periodeIncubation = periodeIncubation;
         this.probaDeces = probaDeces;
+        this.dMax=dMax;
+        this.p0=p0;
     }
+
+
 }
