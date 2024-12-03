4de27b6dde3d2642a20f58304bd0e322741982c8
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-03 16:47:26 +0100

    commit de Variant

---
+++ src/Variant.java
@@ -0,0 +1,6 @@
+public class Variant extends Epidemie{
+    public Variant(String nom, float tauxTransmission, float probaGuerison, int periodeIncubation, double probaDeces)
+    {
+        super(nom, tauxTransmission, probaGuerison, periodeIncubation, probaDeces);
+    }
+}
