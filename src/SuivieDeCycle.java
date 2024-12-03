dbcdeda4be6d1fcfac872e731d930188940ef107
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-03 16:46:55 +0100

    commit de Vaccination

---
+++ src/SuivieDeCycle.java
@@ -0,0 +1,13 @@
+import java.util.List;
+public class SuivieDeCycle {
+    private List historique ;
+
+
+    public void enregistrerCycle(){
+
+    }
+
+    public List getCycleHistorique() {
+        return historique;
+    }
+}
---
+++ src/Vaccination.java
@@ -0,0 +1,12 @@
+public class Vaccination {
+    private int doseType;
+
+    public Vaccination(int doseType) {
+        this.doseType = doseType;
+    }
+
+    public boolean confirmerVacination(){
+
+        return true;
+    }
+}
