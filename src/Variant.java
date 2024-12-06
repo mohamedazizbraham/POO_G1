41548aba040eaa7cbd5b2f52f3dbb02072f40c04
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-06 15:00:36 +0100

    commit de variant

--- src/Variant.java
+++ src/Variant.java
@@ -1,6 +1,14 @@
-public class Variant extends  Maladie {
-    public Variant(String nom, float tauxTransmission, float probaGuerison, int periodeIncubation, double probaDeces,float dMax,float p0)
-    {
+public class Variant extends Maladie {
+    private int idVariant; // Identifiant unique pour différencier les variants
+
+    // Constructeur
+    public Variant(int idVariant, String nom, float tauxTransmission, double probaGuerison, int periodeIncubation, double probaDeces, float dMax, double p0) {
         super(nom, tauxTransmission, probaGuerison, periodeIncubation, probaDeces, dMax, p0);
+        this.idVariant = idVariant;
+    }
+
+    // Getter pour l'identifiant du variant
+    public int getIdVariant() {
+        return idVariant;
     }
 }
