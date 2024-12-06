2d7e64d1b363376962693f055a2e33cc03e9505c
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-06 15:02:08 +0100

    ajout de enregistrerCycle pour enregidtrer les cycles

--- src/SuivieDeCycle.java
+++ src/SuivieDeCycle.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
+
 public class SuivieDeCycle {
-    private List historique ;
+    private List<String> historique;
 
-
-    public void enregistrerCycle(){
-
+    public SuivieDeCycle() {
+        historique = new ArrayList<>();
     }
 
-    public List getCycleHistorique() {
-        return historique ;
+    public void enregistrerCycle(Population population) {
+        int healthy = 0, infected = 0, recovered = 0, diseased = 0;
+        for (Individu individu : population.getIndividus()) {
+            switch (individu.getEtat()) {
+                case Sain -> healthy++;
+                case Infecté -> infected++;
+                case Guéri -> recovered++;
+                case Décédé -> diseased++;
+            }
+        }
+        historique.add("Sain: " + healthy + ", Infecté: " + infected + ", Guéri: " + recovered + ", Décédé: " + diseased);
+    }
+
+    public List<String> getHistorique() {
+        return historique;
     }
 }
