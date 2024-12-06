70a444c43129b68eae56c3a82345770b68694b24
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-06 14:59:18 +0100

    ajout de la méthode recevoirInfection

--- src/Individu.java
+++ src/Individu.java
@@ -1,35 +1,58 @@
 public class Individu {
-private Etat etat;
-private sensibilité sensibilite;
-private int cycleResistance;
-private float x;
-private float y;
-private boolean infecter;
-private boolean guerie;
+    private Etat etat; // HEALTHY, INFECTED, RECOVERED, DISEASED
+    private sensibilité sensibilite; // SENSIBLE, NEUTRE, RESISTANT
+    private boolean vaccine; // Indique si l'individu est vacciné
+    private boolean guerie; // Indique si l'individu est guéri
+    private float x, y; // Position dans l'espace 2D
 
-
-public Individu(Etat etat, float x, float y, String sensitivite) {
-    this.etat = etat;
-    this.x = x;
-    this.y = y;
-    this.sensibilite = sensibilite;
-    this.infecter = false;
-    this.guerie = false;
-    this.cycleResistance = 0;
-}
-public float distance() {
-
-    return 0;
-}
-
-    public String mettreAjourEtat() {
-
-
-        return "";
+    public Individu(Etat etat, sensibilité sensibilite, float x, float y) {
+        this.etat = etat;
+        this.sensibilite = sensibilite;
+        this.x = x;
+        this.y = y;
+        this.vaccine = false;
+        this.guerie = false;
     }
-    public boolean recevoirInfection() {
 
+    public boolean isGuerie() {
+        return guerie;
+    }
 
-        return false;
+    public boolean isVacciné() {
+        return vaccine;
+    }
+
+    public void recevoirVaccination(int doses) {
+        if (doses >= 1) {
+            this.sensibilite = sensibilité.Resistant; // Rend l'individu résistant
+        }
+        if (doses == 2) {
+            this.vaccine = true; // Immunité complète
+        }
+    }
+
+    public Etat getEtat() {
+        return etat;
+    }
+
+    public void recevoirInfection() {
+        if (etat == Etat.Sain) {
+            this.etat = Etat.Infecté;
+        }
+    }
+
+    public void mettreAJourEtat() {
+        if (etat == Etat.Infecté) {
+            if (Math.random() < 0.4) { // Probabilité de guérison
+                this.etat = Etat.Guéri;
+                this.guerie = true;
+            } else if (Math.random() < 0.1) { // Probabilité de décès
+                this.etat = Etat.Décédé;
+            }
+        }
+    }
+
+    public float distance(Individu autre) {
+        return (float) Math.sqrt(Math.pow(this.x - autre.x, 2) + Math.pow(this.y - autre.y, 2));
     }
 }
